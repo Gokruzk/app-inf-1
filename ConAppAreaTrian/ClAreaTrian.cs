5b88e78a670314ef39609dcc7a27a44386fb3050
gokrusk <nigelljama@gmail.com>
2023-04-04 14:45:04 -0500

    Triangule Area

---
+++ ConAppAreaTrian/ClAreaTrian.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAreaTrian
+{
+    internal class ClAreaTrian
+    {
+        int baseT, altura;
+        public ClAreaTrian(int baseT, int altura)
+        {
+            this.baseT = baseT;
+            this.altura = altura;
+        }
+        public double CalcArea()
+        {
+            double area = (baseT * altura)/2;
+            return area;
+        }
+    }
+}
---
+++ ConAppAreaTrian/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using ConAppAreaTrian;
+
+Console.WriteLine("CÁLCULO DE ÁREA DE TRIÁNGULO");
+int baseT, altura;
+Console.Write("Ingrese la base: ");
+baseT = int.Parse(Console.ReadLine());
+Console.Write("Ingrese la altura: ");
+altura  = int.Parse(Console.ReadLine());
+
+ClAreaTrian obj = new ClAreaTrian(baseT, altura);
+
+Console.WriteLine("El área del triángulo calculada es: {0}", obj.CalcArea());
