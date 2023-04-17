87785500169a0bab3344721288d3708a407c77e6
gokrusk <nigelljama@gmail.com>
2023-04-17 08:56:29 -0500

    WinAppTryCatch

---
+++ WinAppTryCatcg/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WinAppTryCatcg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblTittle = new System.Windows.Forms.Label();
+            this.TxtBase = new System.Windows.Forms.TextBox();
+            this.TxtAlt = new System.Windows.Forms.TextBox();
+            this.LblBase = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblMsj = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblTittle
+            // 
+            this.LblTittle.AutoSize = true;
+            this.LblTittle.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTittle.Location = new System.Drawing.Point(174, 9);
+            this.LblTittle.Name = "LblTittle";
+            this.LblTittle.Size = new System.Drawing.Size(151, 28);
+            this.LblTittle.TabIndex = 0;
+            this.LblTittle.Text = "TRY CATCH";
+            // 
+            // TxtBase
+            // 
+            this.TxtBase.Location = new System.Drawing.Point(152, 87);
+            this.TxtBase.Name = "TxtBase";
+            this.TxtBase.Size = new System.Drawing.Size(202, 20);
+            this.TxtBase.TabIndex = 1;
+            this.TxtBase.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // TxtAlt
+            // 
+            this.TxtAlt.Location = new System.Drawing.Point(152, 113);
+            this.TxtAlt.Name = "TxtAlt";
+            this.TxtAlt.Size = new System.Drawing.Size(202, 20);
+            this.TxtAlt.TabIndex = 2;
+            this.TxtAlt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtAlt_KeyPress);
+            // 
+            // LblBase
+            // 
+            this.LblBase.AutoSize = true;
+            this.LblBase.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBase.Location = new System.Drawing.Point(77, 86);
+            this.LblBase.Name = "LblBase";
+            this.LblBase.Size = new System.Drawing.Size(49, 18);
+            this.LblBase.TabIndex = 3;
+            this.LblBase.Text = "Base";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Altura";
+            // 
+            // LblMsj
+            // 
+            this.LblMsj.AutoSize = true;
+            this.LblMsj.Location = new System.Drawing.Point(77, 205);
+            this.LblMsj.Name = "LblMsj";
+            this.LblMsj.Size = new System.Drawing.Size(47, 13);
+            this.LblMsj.TabIndex = 5;
+            this.LblMsj.Text = "Mensaje";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(343, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Cuadro diálogo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 293);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LblMsj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LblBase);
+            this.Controls.Add(this.TxtAlt);
+            this.Controls.Add(this.TxtBase);
+            this.Controls.Add(this.LblTittle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblTittle;
+        private System.Windows.Forms.TextBox TxtBase;
+        private System.Windows.Forms.TextBox TxtAlt;
+        private System.Windows.Forms.Label LblBase;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblMsj;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinAppTryCatcg/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppTryCatcg
+{
+    public partial class Form1 : Form
+    {
+        int a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtAlt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                try
+                {
+                    char letra = TxtAlt.Text[0];
+                    if (letra == 'a' || letra == 'b' || letra == 'c' || letra == 'd' || letra == 'e' || letra == 'f' || letra == 'g' || letra == 'h' || letra == 'i' || letra == 'j' || letra == 'k' || letra == 'l' || letra == 'm' || letra == 'n' || letra == 'o' || letra == 'p' || letra == 'q' || letra == 'r' || letra == 's' || letra == 't' || letra == 'u' || letra == 'v' || letra == 'w' || letra == 'x' || letra == 'y' || letra == 'z')
+                    {
+                        MessageBox.Show("Soy letra");
+                        button1.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Solo ingrese letras");
+                        TxtAlt.Clear();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Ingrese una letra");
+                    TxtAlt.Clear();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A VER DÓNDE SALGO", "Yo dónde saldré", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                try
+                {
+                    a = int.Parse(TxtBase.Text);
+                    if (a >= 100 && a <= 999)
+                    {
+                        TxtAlt.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Solo ingrese números de 3 cifras");
+                        TxtBase.Clear();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Solo ingrese números enteros");
+                    TxtBase.Clear();
+                }
+            }
+        }
+    }
+}
