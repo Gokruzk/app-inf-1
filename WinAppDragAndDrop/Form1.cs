2592ccea79f4939c81b38e2eed541375a76f309e
gokrusk <nigelljama@gmail.com>
2023-04-21 10:57:49 -0500

    WinAppDragAndDrop

---
+++ WinAppDragAndDrop/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppDragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.DoDragDrop(pictureBox1.Image, DragDropEffects.Copy);
+        }
+
+        private void pictureBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+
+        private void pictureBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox2.AllowDrop = true;
+        }
+    }
+}
