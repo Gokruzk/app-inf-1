a2cbdae9e79cf33750c1c786d8142a40ae3ef9f0
gokrusk <nigelljama@gmail.com>
2023-04-25 08:53:10 -0500

    WinAppFactNum

---
+++ WinAppFactorialNum/ClFactorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppFactorialNum
+{
+    internal class ClFactorial
+    {
+        int a;
+        public ClFactorial(int a)
+        {
+            this.a = a;
+        }
+        public int CalcFactorial()
+        {
+            int f = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                f *= i;
+            }
+            return f;
+        }
+    }
+}
---
+++ WinAppFactorialNum/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppFactorialNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                int n = int.Parse(textBox1.Text);
+                ClFactorial obj = new ClFactorial(n);
+                label4.Text = obj.CalcFactorial().ToString();
+            }
+        }
+    }
+}
