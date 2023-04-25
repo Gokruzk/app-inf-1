543a1d607c1c3ee12e342141dea8ff2d5b009130
gokrusk <nigelljama@gmail.com>
2023-04-24 22:49:09 -0500

    WinApp7Afortunado

---
+++ WinApp7Afortunado/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WinApp7Afortunado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.Lbl1 = new System.Windows.Forms.Label();
+            this.Lbl2 = new System.Windows.Forms.Label();
+            this.Lbl3 = new System.Windows.Forms.Label();
+            this.PbxWin = new System.Windows.Forms.PictureBox();
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxWin)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.LblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitle.ForeColor = System.Drawing.Color.Transparent;
+            this.LblTitle.Location = new System.Drawing.Point(135, 19);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(323, 25);
+            this.LblTitle.TabIndex = 0;
+            this.LblTitle.Text = "JUEGO DEL 7 AFORTUNADO";
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl1.ForeColor = System.Drawing.Color.Transparent;
+            this.Lbl1.Location = new System.Drawing.Point(95, 126);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(0, 20);
+            this.Lbl1.TabIndex = 1;
+            // 
+            // Lbl2
+            // 
+            this.Lbl2.AutoSize = true;
+            this.Lbl2.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl2.ForeColor = System.Drawing.Color.Transparent;
+            this.Lbl2.Location = new System.Drawing.Point(262, 126);
+            this.Lbl2.Name = "Lbl2";
+            this.Lbl2.Size = new System.Drawing.Size(0, 20);
+            this.Lbl2.TabIndex = 2;
+            // 
+            // Lbl3
+            // 
+            this.Lbl3.AutoSize = true;
+            this.Lbl3.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl3.ForeColor = System.Drawing.Color.Transparent;
+            this.Lbl3.Location = new System.Drawing.Point(433, 126);
+            this.Lbl3.Name = "Lbl3";
+            this.Lbl3.Size = new System.Drawing.Size(0, 20);
+            this.Lbl3.TabIndex = 3;
+            // 
+            // PbxWin
+            // 
+            this.PbxWin.Image = ((System.Drawing.Image)(resources.GetObject("PbxWin.Image")));
+            this.PbxWin.Location = new System.Drawing.Point(351, 197);
+            this.PbxWin.Name = "PbxWin";
+            this.PbxWin.Size = new System.Drawing.Size(202, 168);
+            this.PbxWin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbxWin.TabIndex = 4;
+            this.PbxWin.TabStop = false;
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlay.Location = new System.Drawing.Point(124, 248);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(144, 54);
+            this.BtnPlay.TabIndex = 5;
+            this.BtnPlay.Text = "JUGAR";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.LblTitle);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(565, 64);
+            this.panel1.TabIndex = 6;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.Location = new System.Drawing.Point(124, 311);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(144, 54);
+            this.BtnReset.TabIndex = 7;
+            this.BtnReset.Text = "REINICIAR";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(565, 412);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnPlay);
+            this.Controls.Add(this.PbxWin);
+            this.Controls.Add(this.Lbl3);
+            this.Controls.Add(this.Lbl2);
+            this.Controls.Add(this.Lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PbxWin)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblTitle;
+        private System.Windows.Forms.Label Lbl1;
+        private System.Windows.Forms.Label Lbl2;
+        private System.Windows.Forms.Label Lbl3;
+        private System.Windows.Forms.PictureBox PbxWin;
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnReset;
+    }
+}
+
---
+++ WinApp7Afortunado/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApp7Afortunado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PbxWin.Visible = false;
+            BtnReset.Visible = false;
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            Random objRandom = new Random();
+            Lbl1.Text = objRandom.Next(1,8).ToString();
+            Lbl2.Text = objRandom.Next(1, 8).ToString();
+            Lbl3.Text = objRandom.Next(1, 8).ToString();
+            if(Lbl1.Text == "7" || Lbl2.Text == "7" || Lbl3.Text == "7")
+            {
+                PbxWin.Visible = true;
+                BtnPlay.Enabled = false;
+                BtnReset.Visible = true;
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Lbl3.Text = "";
+            Lbl2.Text = "";
+            Lbl1.Text = "";
+            BtnPlay.Enabled = true;
+            PbxWin.Visible = false;
+            BtnReset.Visible = false;
+        }
+    }
+}
