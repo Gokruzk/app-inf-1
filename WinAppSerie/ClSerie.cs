980960517070eabbf9dd3624326dffddb5ef01cc
gokrusk <nigelljama@gmail.com>
2023-04-26 12:40:16 -0500

    WinAppSerie

---
+++ WinAppSerie/ClSerie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppSerie
+{
+    internal class ClSerie
+    {
+        int n;
+        public ClSerie(int n)
+        { this.n = n; }
+        public int Factorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            return num *= Factorial(num - 1);
+        }
+        public double Serie(int num)
+        {
+            double resultado = 0;
+            
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    resultado -= Factorial(2 * i) / Math.Pow(2 * i - 1, i);
+                }
+                else
+                {
+                    resultado += Math.Pow(2 * i - 1, i) / Factorial(2 * i);
+                }
+            }
+            return resultado;
+        }
+        public double SerieRecursiva(int num, double resultado, int i)
+        {
+            if (i <= num)
+            {
+                if (i % 2 == 0)
+                {
+                    resultado -= Factorial(2 * i) / Math.Pow(2 * i - 1, i);
+                }
+                else
+                {
+                    resultado += Math.Pow(2 * i - 1, i) / Factorial(2 * i);
+                }
+                resultado = SerieRecursiva(num, resultado, i + 1);
+
+            }
+            return resultado;
+        }
+    }
+
+}
---
+++ WinAppSerie/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppSerie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                if(TxtNum.Text == "")
+                {
+                    MessageBox.Show("Porfavor ingrese un número", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                    TxtNum.Clear();
+                }
+                else
+                {
+                    try
+                    {
+                        int n = int.Parse(TxtNum.Text);
+                        if (n <= 0)
+                        {
+                            MessageBox.Show("Ingrese un valor positivo o mayor a 0", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                            TxtNum.Clear();
+                        }
+                        else if (n > 16)
+                        {
+                            MessageBox.Show("Ingrese un valor menor a 17", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                            TxtNum.Clear();
+                        }
+                        else
+                        {
+                            ClSerie obj = new ClSerie(n);
+                            LblR.Text = obj.SerieRecursiva(n,0,1).ToString();
+                            TxtNum.Clear();
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ingrese un valor menor a 17", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        TxtNum.Clear();
+                    }
+                    
+                }
+            }
+        }
+
+        private void TxtNum_Click(object sender, EventArgs e)
+        {
+            TxtNum.Clear();
+            LblR.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
